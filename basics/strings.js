aaffa511bc2726ed252bb2ca323acdf308ef7fdd
Bello Opeyemi <bellopeyemie@gmail.com>
2023-09-23 17:16:50 +0100

    first commit

---
+++ basics/strings.js
@@ -0,0 +1,11 @@
+// let firstName = "Opeyemi"
+// let lastName = "Bello"
+// let fullName = firstName + " " + lastName
+
+// console.log(fullName)
+
+let city = "Akure"
+let country = "Nigeria"
+let location = city + ", " + country
+
+console.log(location)
