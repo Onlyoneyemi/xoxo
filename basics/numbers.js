a696939f3a40744a50da332d6dab45d4a2078a69
Bello Opeyemi <bellopeyemie@gmail.com>
2023-09-25 12:06:46 +0100

    third commit

---
+++ basics/numbers.js
@@ -0,0 +1,11 @@
+// let age = 24
+// let dogYears = (age + 1) / 4
+
+// console.log(dogYears)
+
+//Challenge Area
+let studentScore = 41
+let maxScore = 100
+let percentage = (studentScore / maxScore) * 100
+
+console.log(percentage)
