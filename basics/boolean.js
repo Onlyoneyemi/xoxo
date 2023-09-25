decefb864d5d496918ba648735d57e1cacab8580
Bello Opeyemi <bellopeyemie@gmail.com>
2023-09-25 15:53:26 +0100

    fourth commit

---
+++ basics/boolean.js
@@ -0,0 +1,27 @@
+// === - Equality symbol
+// !== - Not equal symbol
+// < - Less than symbol
+// > - Greater than symbol
+// <= - Less than and equal to
+// >= Greater than and equal to
+
+// let temp = 132
+
+// if (temp <= 32) {
+//     console.log("It is freezing outside.")
+// }
+
+// if (temp >= 110) {
+//     console.log("It is way too hot outside.")
+// }
+
+
+//Challenge Area
+let age = 7
+ if (age <= 7) {
+    console.log("You will get the minor citizen discount.")
+ }
+
+ if (age >= 65) {
+    console.log("You will get the  senior citizen discount.")
+ }
