9eb62a25663e46dcd09fd0ea90c93c9ba77e225a
Onlyoneyemi <bellopeyemie@gmail.com>
2024-01-10 18:06:13 +0000

    2024-01-10

--- hangman/scripts/request.js
+++ hangman/scripts/request.js
@@ -1,5 +1,5 @@
 const getPuzzle = async (wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     
     if (response.status === 200) {
         const data = await response.json()
@@ -10,7 +10,7 @@ const getPuzzle = async (wordCount) => {
 }
 
 const getCountry = async (countryCode) => {
-    const response = await fetch("http://restcountries.eu/rest/v2/all")
+    const response = await fetch("https://restcountries.eu/rest/v2/all")
 
     if (response.status === 200) {
         const data = await response.json()
